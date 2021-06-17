20e62f7e377e63e77de8dc02e7695f958e000f78
rmartins <rmartins@student.42lisboa.com>
2021-06-17 07:58:17 +0100

    test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 make
 SERVER=`ps aux | grep " ./server" | head -n 1 | awk -F ' ' '{print $2}'`
 
-./client $SERVER "romeu"
-./client $SERVER "rita"
 ./client $SERVER "0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789"
+./client $SERVER "romeu\n"
+./client $SERVER "rita\n"
